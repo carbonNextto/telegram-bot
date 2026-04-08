74b278472e863b6708eb9858fcf9fc73405af2d4
carbonNextto <carbontts123@gmail.com>
2026-04-08 23:33:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
+  polling: true
+});
 
 bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, "บอททำงานแล้ว 🚀");
+  bot.sendMessage(msg.chat.id, "🔥 OpenClaw Bot ทำงานแล้ว!");
 });
